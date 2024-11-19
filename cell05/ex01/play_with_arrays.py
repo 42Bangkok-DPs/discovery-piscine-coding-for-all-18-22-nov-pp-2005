b72dc4a2753b240684b103e720e885e8608275f5
pp-2005 <2bm2p.andej@gmail.com>
2024-11-19 20:59:56 +0700

    append

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,10 +1,8 @@
 ori = [2, 8, 9, 48, 8, 22, -12, 2]
 new = []
 
-index=0
-while index<len(ori):
-    new.append(ori[index]+2)
-    index+=1
+for i in range(len(ori)):
+    new.append(ori[i]+2)
 
 print(ori)
 print(new)
