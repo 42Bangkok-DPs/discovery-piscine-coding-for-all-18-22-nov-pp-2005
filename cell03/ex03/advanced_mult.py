c664e98bc3e534f7e3e7328828ea2eba67112b84
pp-2005 <121081365+pp-2005@users.noreply.github.com>
2024-11-20 15:35:57 +0000

    sys

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,7 +1,15 @@
 #!/bin/python3
+import sys
 
-for i in range(0,11):
-    print(f"Teble de {i}:", end=" ")
-    for j in range(0,11):
-        print(i*j, end=" ")
-    print()
+if len(sys.argv) == 1:
+    i=0
+    while i<11:
+        print(f"Teble de {i}:", end=" ")
+        j=0
+        while j<11:
+            print(i*j, end=" ")
+            j+=1
+        print()
+        i+=1
+else:
+    print("none")
