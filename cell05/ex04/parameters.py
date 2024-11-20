b9289ad63a90b44a1247d26715ad6edea72edf09
pp-2005 <121081365+pp-2005@users.noreply.github.com>
2024-11-20 14:42:50 +0000

    edit

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,9 +1,4 @@
 #!/bin/python3
-
 import sys
 
-# จำนวน arguments (ยกเว้นชื่อไฟล์ script)
-num_of_params = len(sys.argv) - 1
-
-# แสดงผล
-print(f"Number of parameters: {num_of_params}.")
+print(f"Number of parameters: {len(sys.argv) - 1}.")
